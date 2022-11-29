7b84a7d6cd8daab3acb6fe1065891ad07f44b917
Jakub Matejka <jkm@spectroinlets.com>
2022-11-29 13:03:33 +0100

    task 1 and 2

--- src/database.py
+++ src/database.py
@@ -1,5 +1,8 @@
 import csv
 
+from src.reminder import PoliteReminder
+
+
 def list_reminders():
     f = open("reminders.csv", "r")
 
@@ -12,8 +15,11 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
+
 def add_reminder(text):
+    reminder = PoliteReminder(text)
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow([reminder.text])
+
