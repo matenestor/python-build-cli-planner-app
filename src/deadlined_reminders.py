5a21b500452a5a95050c412e411687fc0485ffb7
Jakub Matejka <jkm@spectroinlets.com>
2022-11-29 13:27:48 +0100

    task 6

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,21 +1,20 @@
 from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
+from datetime import datetime
 
 from dateutil.parser import parse
 
 
-# - not all methods on an Abstract Base Class need to be abstract,
-#   however, if none are abstract, then the class itself is no longer abstract
-# - since python 3.4 you can also create an Abstract Base Class by inheriting
-#   from the ABC class of the abc module
-
-
+# not all methods on an Abstract Base Class need to be abstract,
+# however, if none are abstract, then the class itself is no longer abstract
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
     def is_due(self):
         pass
 
 
+# since python 3.4 you can also create an Abstract Base Class
+# by inheriting from the ABC class of the abc module
 class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
     def is_due(self):
@@ -26,4 +25,7 @@ class DateReminder(DeadlinedReminder):
     def __init__(self, text, date):
         self.date = parse(date, dayfirst=True)
         self.text = text
+
+    def is_due(self):
+        return self.date <= datetime.now()
 
