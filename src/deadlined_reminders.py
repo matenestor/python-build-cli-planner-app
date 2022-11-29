f8eb0fd2d3f96c37c1231ef392afb855b604596d
Jakub Matejka <jkm@spectroinlets.com>
2022-11-29 13:20:55 +0100

    task 4

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,12 +1,21 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
 
 
-# not all methods on an Abstract Base Class need to be abstract
-# however, if none are abstract, then the class itself is no longer abstract
+# - not all methods on an Abstract Base Class need to be abstract,
+#   however, if none are abstract, then the class itself is no longer abstract
+# - since python 3.4 you can also create an Abstract Base Class by inheriting
+#   from the ABC class of the abc module
 
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
     def is_due(self):
         pass
+
+
+class DeadlinedReminder(Iterable, ABC):
+    @abstractmethod
+    def is_due(self):
+        pass
+
