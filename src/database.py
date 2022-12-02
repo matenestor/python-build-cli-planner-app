2ab704d149f9e052d146e93c37d63aa8eebf81cf
Jakub Matejka <jkm@spectroinlets.com>
2022-12-02 10:21:46 +0100

    task 11

--- src/database.py
+++ src/database.py
@@ -17,10 +17,16 @@ def list_reminders():
 
 
 def add_reminder(text, date, ReminderClass):
-    if not issubclass(ReminderClass, DeadlinedReminder):
-        raise TypeError("Invalid Reminder Class")
-
     reminder = ReminderClass(text, date)
+
+    # Check whether an instance of a ReminderClass class is valid,
+    # as opposed to the class itself done with 'issubclass()'.
+    # Thus, do not make an assumption about the parameters taken by the
+    # constructor ReminderClass(). Using 'isinstance()' would allow the
+    # add_reminder() function to TODO? receive the instance directly, thus
+    # delegating its construction to a code that knows how to do it better.
+    if not isinstance(reminder, DeadlinedReminder):
+        raise TypeError("Invalid Reminder Class")
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
